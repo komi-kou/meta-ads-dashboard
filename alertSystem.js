6891e1c3d33eb282b15b6a65d5ea340b791de071
komi-kou <komiya11122@gmail.com>
2025-07-25 11:40:15 +0900

    Add files via upload

--- alertSystem.js
+++ alertSystem.js
@@ -198,7 +198,9 @@ async function checkMetricAlert(metric, rule, historicalData, goalType) {
             case 'above_baseline':
                 alertTriggered = await checkCPMBaseline(rule, historicalData);
                 if (alertTriggered) {
-                    alertMessage = `CPMがベースラインから+${rule.threshold}円上がった状態が${rule.days}日間続いています`;
+                    const targetCPM = await getTargetCPM();
+                    const thresholdCPM = targetCPM + rule.threshold;
+                    alertMessage = `CPMが目標${targetCPM.toLocaleString()}円から+${rule.threshold}円上昇し${thresholdCPM.toLocaleString()}円以上の状態が${rule.days}日間続いています`;
                     severity = 'warning';
                 }
                 break;
@@ -206,7 +208,9 @@ async function checkMetricAlert(metric, rule, historicalData, goalType) {
             case 'above_target':
                 alertTriggered = await checkCPATarget(rule, historicalData);
                 if (alertTriggered) {
-                    alertMessage = `CPAが目標の${rule.threshold}%を超えた状態が${rule.days}日間続いています`;
+                    const targetCPA = await getTargetCPA();
+                    const thresholdCPA = Math.round(targetCPA * (rule.threshold / 100));
+                    alertMessage = `CPAが目標${targetCPA.toLocaleString()}円の${rule.threshold}%（${thresholdCPA.toLocaleString()}円）を超えた状態が${rule.days}日間続いています`;
                     severity = 'critical';
                 }
                 break;
